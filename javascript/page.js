ca30f8fe531d8b9064ed0f7745cde76037270afb
OTSFan203 <45924049+otsfan203@users.noreply.github.com>
2019-01-28 19:38:26 -0500

    Delete page.js

--- javascript/page.js
+++
@@ -1,3 +0,0 @@
-function nebraskaFunc() {
-  confirm("Work on Exemplum has only just began and currently Exemplum is available online. If you wish to be notified when Exemplum is released, please ask OTSFan203. Clicking OK will bring you to the webpage where Exemplum's editor will be, but is not yet.");
-}
